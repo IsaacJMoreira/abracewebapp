c6154de41dc63c70e61957cc34845839ddf6e513
Isaac J Moreira <120274547+IsaacJMoreira@users.noreply.github.com>
2023-10-17 18:32:02 -0300

    ADD: event card component

---
+++ front/app/volunteer/page.tsx
@@ -0,0 +1,113 @@
+"use client";
+import { InstagramLogoIcon, Link1Icon } from "@radix-ui/react-icons";
+import {
+  Card,
+  Flex,
+  Text,
+  Button,
+  Inset,
+  IconButton,
+  TextField,
+  Separator,
+  Select,
+} from "@radix-ui/themes";
+import NextImage from "next/image";
+import Volunteer from "../../public/assets/images/Volunteer.jpg";
+import TemporaryHomeIcon from "../../public/assets/icons/3203239 1.png";
+import STDButton from "@components/stdButton/STDButton";
+import VOLUNTEERIcon from "../../public/assets/icons/volunteer-icon-10 1.png";
+import DONATEIcon from "../../public/assets/icons/2904845 1.png";
+import Link from "next/link";
+import { useState } from "react";
+
+const totalPets = "30";
+
+const Home = () => {
+  const [subscription, setSubscription] = useState(false);
+  const [plano, setPlano] = useState("");
+  return (
+    <div>
+      <Card className="w-screen ">
+        <Inset clip="padding-box" side="top" pb="current">
+          <NextImage
+            className="w-screen"
+            src={Volunteer}
+            alt="Filhote de cachorro de pelo marrom lambendo a tela"
+          />
+        </Inset>
+
+        <Flex align="center" direction="column" justify="between" gap="4">
+          <Card className="w-11/12">
+            <Flex align="center" direction="column" justify="between" gap="4">
+             
+
+             
+                  <Text as='div' size='3' weight='bold' align='center' color="lime"> INSCREVA-SE NO NOSSO PROGRAMA DE LAR TEMPORÁRIO</Text>
+                <Text as="div" size='1' align='center'>
+                Dar lar temporário a um animal abandonado é uma responsabilidade
+                que implica cuidados físicos e emocionais, exigindo tempo e
+                dedicação. É uma oportunidade de transformar a vida de um ser
+                indefeso, proporcionando amor, cuidado e paciência. Além disso,
+                desempenha um papel fundamental na luta contra o abandono de
+                animais, inspirando outros a adotarem e mostrando que todos
+                merecem uma segunda chance. É um ato de compaixão e empatia que
+                recompensa tanto o animal quanto o cuidador.
+              </Text>
+              <Text as='div' color="tomato">AQUI DEVE ESTAR A POLÍTICA DE ADOÇÃO</Text>
+
+              <STDButton
+                iconAlt="gerar pix"
+                iconNI={TemporaryHomeIcon}
+                text="quero me inscrever"
+                callBack={()=>alert("Inscrição no programa 'LAR TEMPORÁRIO' enviada")}
+              />             
+
+            
+              <Flex align="center" direction="column" justify="between" gap="4">
+                <Text align="center" size="3" weight="medium" as="div">
+                  continue nos ajudando
+                </Text>
+                <Flex align="center" direction="row" justify="between" gap="4">
+                  <Link href="/help">
+                    <STDButton
+                      iconAlt="Doe"
+                      iconNI={DONATEIcon}
+                      text="quero doar"
+                    />
+                  </Link>
+                  <STDButton
+                    iconAlt="vountariar"
+                    iconNI={VOLUNTEERIcon}
+                    text="voluntariar"
+                  />
+                </Flex>
+              </Flex>
+              <Text align="center" size="1" weight="bold" as="div">
+                entre em contato conosco através das nossas redes
+              </Text>
+              <IconButton>
+                <InstagramLogoIcon width="20" height="20" />
+              </IconButton>
+            </Flex>
+          </Card>
+
+          <Flex align="center" direction="column" justify="between" gap="4">
+            <Text align="center" size="1" weight="bold" as="div">
+              temos vários amiguinhos esperando um novo lar. <br /> Vamos
+              conhecê-los?
+            </Text>
+            <Link href="/adoption">
+              <Button style={{ width: "75vw", maxWidth: 340 }} size="3">
+                ADOTAR
+              </Button>
+            </Link>
+          </Flex>
+          <br />
+        </Flex>
+        <br />
+      </Card>
+    </div>
+  );
+};
+
+export default Home;
---
+++ front/components/EventCard/EventCard.tsx
@@ -0,0 +1,200 @@
+"use client";
+import {
+  Card,
+  Flex,
+  Box,
+  Text,
+  Button,
+  ScrollArea,
+  Inset,
+  Dialog,
+  Separator,
+  TextField,
+  Link,
+} from "@radix-ui/themes";
+import NextImage from "next/image";
+import FrontDog from "../public/assets/images/FrontDog.png";
+import STDButton from "@components/stdButton/STDButton";
+import DONATEIcon from "../../public/assets/icons/2904845 1.png";
+import VOLUNTEERIcon from "../../public/assets/icons/volunteer-icon-10 1.png";
+import { FC, ReactNode, useEffect } from "react";
+import React from "react";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
+import {
+  CheckIcon,
+  ChevronLeftIcon,
+  Cross1Icon,
+  HeartFilledIcon,
+  LockClosedIcon,
+  PersonIcon,
+} from "@radix-ui/react-icons";
+
+interface IMGProps {
+  URL?: string;
+  ALT?: string;
+}
+
+interface MyProps {
+  children?: ReactNode;
+  name: string;
+  date: string;
+  time: string;
+  duration: string;
+  location: string;
+  description: string;
+  imgURL: IMGProps;
+  linkToSocialMedia: string;
+  URL: string;
+  ALT: string;
+}
+
+const Disclaimer = () => {
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger>
+        <Button variant="surface">ADOTAR</Button>
+      </Dialog.Trigger>
+
+      <Dialog.Content>
+        <Dialog.Title size="6">Atenção</Dialog.Title>
+
+        <Flex direction="column" justify="between" gap="3">
+          <Flex direction="column" gap="3">
+            <Text as="div" size="2">
+              Honrar um compromisso marcado é de fundamental importância,
+              principalmente quando se trata de voluntariado em uma organização
+              como a ONG ABRACE, que despende esforços incansáveis para
+              encontrar lares amorosos para animais de estimação abandonados.
+              <br/>Cada pessoa que se dispõe a doar seu tempo e esforço desempenha um
+              papel crucial na realização dessa nobre missão. Ao cumprir um
+              compromisso marcado, você não apenas demonstra respeito pelos
+              coordenadores e colegas voluntários, mas também contribui para o
+              bem-estar dos animais que dependem dessa ajuda. <br/>No entanto,
+              compreendemos que imprevistos podem ocorrer, e é por isso que
+              pedimos a todos os voluntários que, caso precisem desistir da
+              presença em um compromisso, nos avisem com pelo menos 24 horas de
+              antecedência, permitindo-nos ajustar nossa programação e garantir
+              que nossos resgates e cuidados aos animais continuem de forma
+              eficaz e organizada. Cada esforço conjunto é valioso na luta
+              contra o abandono de animais de estimação, e sua dedicação faz a
+              diferença.
+            </Text>
+            <Flex direction="column" justify="between" align="center" gap="3">
+              <Text as="div" align="center" size="3" weight="medium">
+                ainda não tem certeza?
+              </Text>
+              <Flex align="center" direction="row" justify="between" gap="4">
+                <Dialog.Close>
+                  <Button variant="surface">
+                    <ChevronLeftIcon width="18" height="18" />
+                    voltar
+                  </Button>
+                </Dialog.Close>
+                <Link href="/help">
+                  <Button variant="surface">
+                    <NextImage className=" w-7" alt="Doe" src={DONATEIcon} />
+                    doar
+                  </Button>
+                </Link>
+               
+              </Flex>
+
+              
+
+              <Dialog.Close>
+                <Button variant="surface" className="w-32">
+                  <HeartFilledIcon width="18" height="18" />
+                  sim, quero me inscrever
+                </Button>
+              </Dialog.Close>
+            </Flex>
+          </Flex>
+        </Flex>
+      </Dialog.Content>
+    </Dialog.Root>
+  );
+};
+
+const EventCard: FC<MyProps> = (props) => {
+  return (
+    <Card className="max-w-sm " variant="surface">
+      <Inset clip="padding-box" side="top" pb="current">
+        <NextImage width="384" height="100" src={props.URL} alt={props.ALT} />
+      </Inset>
+
+      <Flex direction="column" justify="between" gap="2">
+        <ScrollArea
+          type="always"
+          scrollbars="horizontal"
+          style={{ width: "100%", padding: "5%" }}
+        >
+          <Flex
+            className="p-1"
+            direction="row"
+            justify="between"
+            align="center"
+            gap="3"
+            wrap="nowrap"
+          >
+            
+
+            
+
+            <Card variant="classic" className="w-20 h-16">
+              <Text as="div" size="3" weight="bold" align="center">
+                {props.date}
+              </Text>
+              <Text
+                as="div"
+                size="1"
+                weight="medium"
+                align="center"
+                color="lime"
+              >
+                data
+              </Text>
+            </Card>
+
+            <Card variant="classic" className="w-20 h-16">
+              <Text as="div" size="3" weight="bold" align="center">
+                {props.time}
+              </Text>
+              <Text
+                as="div"
+                size="1"
+                weight="medium"
+                align="center"
+                color="lime"
+              >
+                hora
+              </Text>
+            </Card>
+            <Card variant="classic" className="w-20 h-16">
+              <Text as="div" size="6" weight="bold" align="center">
+                {props.duration}
+              </Text>
+              <Text
+                as="div"
+                size="1"
+                weight="medium"
+                align="center"
+                color="lime"
+              >
+                duração
+              </Text>
+            </Card>
+          </Flex>
+        </ScrollArea>
+        <Text as="div" size="8" weight="bold" color="lime">
+          {props.name}
+        </Text>
+        <Text as="div" size="3">
+          {props.description}
+        </Text>
+        <Disclaimer />
+      </Flex>
+    </Card>
+  );
+};
+
+export default EventCard;
